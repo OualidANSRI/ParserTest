ede687c88cf33a91857103e167fafc3c823940b9
Oualid Ansri <oualid.ansri@gmail.com>
2021-05-20 17:19:33 +0200

    Ajoutez des fichiers projet.

---
+++ ParserTest/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace ParserTest
+{
+    partial class RNC
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chiffreEntree = new System.Windows.Forms.TextBox();
+            this.ChiffeNumerique = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // chiffreEntree
+            // 
+            this.chiffreEntree.Location = new System.Drawing.Point(172, 164);
+            this.chiffreEntree.Name = "chiffreEntree";
+            this.chiffreEntree.Size = new System.Drawing.Size(125, 27);
+            this.chiffreEntree.TabIndex = 0;
+            this.chiffreEntree.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // ChiffeNumerique
+            // 
+            this.ChiffeNumerique.AutoSize = true;
+            this.ChiffeNumerique.Location = new System.Drawing.Point(112, 113);
+            this.ChiffeNumerique.Name = "ChiffeNumerique";
+            this.ChiffeNumerique.Size = new System.Drawing.Size(245, 20);
+            this.ChiffeNumerique.TabIndex = 1;
+            this.ChiffeNumerique.Text = "Veuillez saisir un chiffre numérique :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(190, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // RNC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 453);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ChiffeNumerique);
+            this.Controls.Add(this.chiffreEntree);
+            this.Name = "RNC";
+            this.Text = "Roman Numerical Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox chiffreEntree;
+        private System.Windows.Forms.Label ChiffeNumerique;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ParserTest/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParserTest
+{
+    public partial class RNC : Form
+    {
+
+         public static String valstr = "";
+        short value = 0; 
+        public RNC()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            char[] t = null;
+            valstr = chiffreEntree.Text;
+            t = valstr.ToCharArray();
+            int i=0;
+            /*do
+            {
+                MessageBox.Show("Veuillez saisir une valeur correcte!", "Alerte");
+                i++;
+            } while (t[i] != 'I' || t[i] != 'V' || t[i] != 'X' || t[i] != 'L' || t[i] != 'C' || t[i] != 'D' || t[i] != 'M');
+            */
+          MessageBox.Show(string.Format("La valeur en valeur romane est :{0}", romanValue()), "Alerte", MessageBoxButtons.OK);
+        }
+        short romanValue()
+        {
+            Dictionary<char, short> valchar = new Dictionary<char, short>();
+            valchar.Add('I', 1);
+            valchar.Add('V', 5);
+            valchar.Add('X', 10);
+            valchar.Add('L', 50);
+            valchar.Add('C', 100);
+            valchar.Add('D', 500);
+            valchar.Add('M', 1000);
+            short total = 0;
+            short last_value = 0;
+            short new_value = 0;
+
+
+            for (int i = 0; i <= valstr.Length - 2; i = i + 2)
+            {
+                short t = valchar.GetValueOrDefault(valstr[i + 1]);
+                new_value = valchar.GetValueOrDefault(valstr[i]);
+
+                if (new_value < t)
+                {
+                    total = (short) (total+ valchar.GetValueOrDefault(valstr[i + 1]) - new_value);
+                    //new_value = valchar.GetValueOrDefault(valstr[i]);
+                  //  last_value = new_value;
+                }
+                else
+                {
+                   //new_value = (short)(valchar.GetValueOrDefault(valstr[i - 1]));
+                    total = (short)(total + valchar.GetValueOrDefault(valstr[i + 1]) + new_value);                    //last_value = new_value;
+                }
+            }
+
+            return total;
+        }
+    }
+}
